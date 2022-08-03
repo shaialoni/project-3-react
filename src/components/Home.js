2007b8c277b54ae4bca51ff4a4ea43eb6e939c73
shaialoni <gitShai2022>
2022-08-03 14:57:06 -0700

    Fixed link error in Header Added file input form + button Added handleSubmit and handleChange of file form  - store file in state on change  - make POST request using FormData on submit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,43 @@
 import { Form, Button } from 'react-bootstrap'
+import { useState } from 'react'
+import axios from 'axios'
+import apiUrl from './../apiConfig'
+
 
 const Home = (props) => {
 	// const { msgAlert, user } = props
-	console.log('props in home', props)
+	// console.log('props in home', props)
 
+	const [ selected, setSelected ] = useState(null)
+
+	const handleChange = (e) => {
+		e.preventDefault()
+		setSelected(e.target.files[0])
+	}
+
+	const handleSubmit =(e) => {
+		e.preventDefault()
+		const data = new FormData()
+		data.append('upload', selected)
+		axios({
+			url: `${apiUrl}/uploads`,
+			method: 'POST',
+			//short for data: data
+			data
+		})
+			.then(console.log)
+			.catch(console.error)
+	}
 	return (
 		<>
 			<h2>Home Page</h2>
 			{/* form for file input */}
-			<Form>
-			<Form.Group controlId="formFile" className="mb-3">
+			<Form onSubmit={handleSubmit}>
+			<Form.Group className="mb-3">
 				<Form.Label>Default file input example</Form.Label>
-				<Form.Control type="file" />
+				<Form.Control type="file" onChange={handleChange} />
 			</Form.Group>
-			<Button type="submit" variant="outline-secondary">Upload File</Button>
+			<Button type="submit" variant="outline-secondary">Secondary</Button>
 			</Form>
 		</>
 	)
