14ecdabe130a5de54a7a7e627aa0f262438f4749
shaialoni <gitShai2022>
2022-08-03 13:44:11 -0700

    updated package.json added input file form / button added .gitignore

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,3 +1,5 @@
+import { Form, Button } from 'react-bootstrap'
+
 const Home = (props) => {
 	// const { msgAlert, user } = props
 	console.log('props in home', props)
@@ -5,6 +7,14 @@ const Home = (props) => {
 	return (
 		<>
 			<h2>Home Page</h2>
+			{/* form for file input */}
+			<Form>
+			<Form.Group controlId="formFile" className="mb-3">
+				<Form.Label>Default file input example</Form.Label>
+				<Form.Control type="file" />
+			</Form.Group>
+			<Button type="submit" variant="outline-secondary">Upload File</Button>
+			</Form>
 		</>
 	)
 }
